99f823fb31684ce781abc3e6167af343ac7650c1
lesonhai0339 <haileds939@gmail.com>
2024-01-12 23:06:21 +0700

    up

---
+++ CleanArchitecture.Application/OrderDetails/Commands/Update/UpdateOrderDetailCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.OrderDetails.Commands.Update
+{
+    public class UpdateOrderDetailCommandHandler : IRequestHandler<UpdateOrderDetailCommand, bool>
+    {
+        private readonly IOrderDetailRepository _orderDetailRepository;
+        public UpdateOrderDetailCommandHandler(IOrderDetailRepository orderDetailRepository)
+        {
+            _orderDetailRepository = orderDetailRepository;
+        }
+        public async Task<bool> Handle(UpdateOrderDetailCommand request, CancellationToken cancellationToken)
+        {
+            var orderdetail = await _orderDetailRepository.FindByIdAsync(request.Id, cancellationToken);
+            if (orderdetail == null)
+            {
+                throw new DirectoryNotFoundException("OrderDetail does not exits");
+            }
+            orderdetail.UnitPrice = request.UnitPrice;
+            orderdetail.Quantity = request.Quantity;
+            orderdetail.Price = request.Price;
+            _orderDetailRepository.Update(orderdetail);
+            var result = await _orderDetailRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
+            if(result> 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CleanArchitecture.Domain/Repositories/IOrderDetailRepository.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Repositories
+{
+    public interface IOrderDetailRepository: IEFRepository<OrderDetail, OrderDetail>
+    {
+        Task<OrderDetail?> FindByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<List<OrderDetail>?> FindAll(CancellationToken cancellationToken = default);
+        Task<List<OrderDetail>> FindByIdsAsync(Guid[] ids, CancellationToken cancellationToken = default);
+    }
+}
